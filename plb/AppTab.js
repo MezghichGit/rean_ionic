462335562fc64fd4fc5dc6a22349e651ad8c7495
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-05-28 13:46:52 +0100

    systeme de navigation en tab

--- plb/AppTab.js
+++ plb/AppTab.js
@@ -4,15 +4,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AccueilScreen from './screens/AccueilScreen';
 import ContactScreen from './screens/ContactScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const tab = createBottomTabNavigator();
 
 export default function App() {
 
   return (
     <NavigationContainer>
-      <tab.Navigator>
+      <tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name == "Home") { iconName = "home-outline"; }
+            else if (route.name == "Setting") { iconName = "settings-outline"; }
+        
+
+            return (<Ionicons name={iconName} color={'red'} size={20}/> );
+          }
+        })}>
+
+
+
        <tab.Screen name="Home" component={AccueilScreen} options={{ title: 'Accueil' }}/> 
-       <tab.Screen name="Contact" component={ContactScreen} options={{ title: 'Call us' }}/> 
+       <tab.Screen name="Setting" component={ContactScreen} options={{ title: 'Call us' }}/> 
       </tab.Navigator>
     </NavigationContainer>
   );

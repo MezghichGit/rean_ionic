ba63366cd53ac8de756e728eb46dc9a46fbda5dd
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-05-28 15:44:01 +0100

    correction exo consommation api

--- plb/screens/NewspaperScreen.js
+++ plb/screens/NewspaperScreen.js
@@ -1,11 +1,48 @@
 
 import { StatusBar } from 'expo-status-bar';
-import { Text, View,StyleSheet } from 'react-native';
+import { Text, View,StyleSheet,ActivityIndicator, ScrollView } from 'react-native';
+import { useEffect, useState } from 'react';
 function NewspaperScreen() {
-    return (<View style={styles.container}>
-      <Text>Newspapers</Text>
+
+
+  const[albums,setAlbums]=useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  const getData=()=>{
+    fetch('https://jsonplaceholder.typicode.com/albums')
+    .then(response=>response.json())
+    .then(data=>{
+      //console.log(data);
+      setAlbums(data);
+     // setIsLoading(false);
+     setTimeout(() => {
+        setIsLoading(false);
+    }, 1000);
+    })
+  }
+  useEffect(() => {
+      getData();
+    },[])
+
+    
+
+    if(isLoading){
+      return(
+          <ScrollView >
+              <ActivityIndicator size="large"/>
+          </ScrollView>
+      );
+  } else {
+    return (<ScrollView >
+    
+      <Text>Welcome sNewspapers screen</Text>
       <StatusBar style="auto" />
-    </View>);
+      <View style={styles.container}>
+        {albums.map(_albums=><Text key={_albums.id}>{_albums.userId}***{_albums.title}</Text>)}
+      </View>
+    </ScrollView>)};
+
+    
   }
   const styles = StyleSheet.create({
     container: {

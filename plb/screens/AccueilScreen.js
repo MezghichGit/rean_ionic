d4cb083adc9b309ae7ade29e5d377de01fba693e
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-05-28 15:59:59 +0100

    utilisation de async/await

--- plb/screens/AccueilScreen.js
+++ plb/screens/AccueilScreen.js
@@ -1,51 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View,StyleSheet,ActivityIndicator } from 'react-native';
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 import { useEffect, useState } from 'react';
 function AccueilScreen() {
 
-    const[users,setUsers]=useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        getData();
-      },[])
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getData();
+  }, [])
 
-      const getData=()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>response.json())
-        .then(data=>{
-          //console.log(data);
-          setUsers(data);
-         // setIsLoading(false);
-         setTimeout(() => {
-            setIsLoading(false);
-        }, 1000);
-        })
-      }
-
-      if(isLoading){
-        return(
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <ActivityIndicator size="large"/>
-            </View>
-        );
-    } else {
-      return (<View style={styles.container}>
-        {console.log(users)}
-        <Text>Welcome screen</Text>
-        <StatusBar style="auto" />
-        <View style={styles.container}>
-          {users.map(_user=><Text key={_user.id}>{_user.name}***{_user.email}</Text>)}
-        </View>
-      </View>)};
-    
+  /* const getData=()=>{
+     fetch('https://jsonplaceholder.typicode.com/users')
+     .then(response=>response.json())
+     .then(data=>{
+       //console.log(data);
+       setUsers(data);
+      // setIsLoading(false);
+      setTimeout(() => {
+         setIsLoading(false);
+     }, 1000);
+     })
+   }*/
+  const getData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    setUsers(data);
+    setTimeout(() => {
+      setIsLoading(false);
+  }, 1000);
   }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
-  export default AccueilScreen;
+
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  } else {
+    return (<View style={styles.container}>
+      
+      <Text>Welcome screen</Text>
+      <StatusBar style="auto" />
+      <View style={styles.container}>
+        {users.map(_user => <Text key={_user.id}>{_user.name}***{_user.email}</Text>)}
+      </View>
+    </View>)
+  };
+
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default AccueilScreen;

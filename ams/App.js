f9d96113cb594857c65cb82cee1e29f4100e3cb2
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-05-30 12:39:22 +0100

    version avec push notify

--- ams/App.js
+++ ams/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View ,SafeAreaView,Image} from 'react-native';
 import Navigation from './navigation';
+import registerNNPushToken from 'native-notify';
 export default function App() {
+  registerNNPushToken(21579, '7ohbRkDJaScp13QqG31kgP');
   return (
     <SafeAreaView style={styles.container}>
      <Navigation />
